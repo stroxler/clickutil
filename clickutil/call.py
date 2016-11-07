cb4ee4546f8c6b009e8dd70e1f9302667bf762ee
Steven Troxler <steven.troxler@gmail.com>
2016-11-07 09:52:50 -0800

    Add a `use_output` decorator, fix test bug
     - Added a use_output decorator, which is similar to call but
       lets you use the output of the function you are directing to.
       This is useful if you want to have parallel python and command
       line apis, in cases where you need to convert a python list
       or dict return into print statements or writing a json/yaml
       file when on the command line
     - In the process I discovered a test bug where I wasn't calling
       the right function in `test_call`, which I fixed.

--- clickutil/call.py
+++ clickutil/call.py
@@ -4,21 +4,21 @@ Base decorators for use with clickutil.
 import wrapt
 
 
-def call(f):
+def call(target):
     """
-    Tool to wrap a call to `f` as a decorator on a placeholder function.
+    Tool to wrap a call to `target` as a decorator on a placeholder function.
 
-    This is useful because if you decorate `f` directly, then it becomes
+    This is useful because if you decorate `target` directly, then it becomes
     unavailable in the namespace where you do the decoration, whereas
     decorating a placeholder works well.
 
     For example:
 
-    @call(f)
-    def _f(): pass
+    @call(target)
+    def _target(): pass
 
-    Can be used to wrap a call to f inside a click command without loosing
-    `f` in the current namespace.
+    Can be used to wrap a call to `target` inside a click command without
+    loosing `target` in the current namespace.
 
     """
     def decorator(placeholder):
@@ -26,8 +26,42 @@ def call(f):
         def make_wrapper(wrapped, instance, args, kwargs):
             return wrapped(*args, **kwargs)
 
-        wrapper = make_wrapper(f)
+        wrapper = make_wrapper(target)
         wrapper.__name__ = placeholder.__name__
         wrapper.__module__ = placeholder.__module__
         return wrapper
+    return decorator
+
+
+def use_output(target):
+    """
+    Tool to wrap a call to `target` as a decorator on a function that
+    does something with its output.
+
+    This is similar to the `call` decorator, except that it lets the
+    click endpoint do something custom with the output. This is useful
+    if you have a python api that returns information about something,
+    for example as a list or dict, but the command-line interface
+    needs to print that data.
+
+    For example:
+
+    @call(target)
+    def _target(output):
+        for thing in output:
+            print(thing)
+
+    """
+    def decorator(placeholder):
+        @wrapt.decorator
+        def make_wrapper(wrapped, instance, args, kwargs):
+            output = wrapped(*args, **kwargs)
+            placeholder(output)
+            return output
+
+        wrapper = make_wrapper(target)
+        wrapper.__name__ = placeholder.__name__
+        wrapper.__module__ = placeholder.__module__
+        return wrapper
+
     return decorator
--- clickutil/tests/test_call.py
+++ clickutil/tests/test_call.py
@@ -1,5 +1,5 @@
 from ..argspec import get_argspec
-from ..call import call
+from ..call import call, use_output
 
 
 def test_call():
@@ -13,7 +13,30 @@ def test_call():
     # check that _f has the docstring, name, and argspec of f
     assert _f.__doc__ == "f documentation", "docstring preserved"
     assert 'x' in get_argspec(_f).args, "argspec preserved"
+    assert _f.__name__ == "_f"
     # verify that calls to _f get redirected to f
     expected = 'x is 5'
-    actual = f(5, None)
+    actual = _f(5, None)
     assert actual == expected, "call redirected"
+
+
+def test_use_output():
+    def f(x, y):
+        "f documentation"
+        return x + y
+
+    outputs = []
+
+    @use_output(f)
+    def _f(output):
+        outputs.append(output)
+
+    # check that _f has the docstring, name, and argspec of f
+    assert _f.__doc__ == "f documentation", "docstring preserved"
+    assert 'x' in get_argspec(_f).args, "argspec preserved"
+    assert _f.__name__ == "_f"
+    # verify that calls to _f get redirected to f
+    expected = 3
+    actual = _f(1, 2)
+    assert actual == expected, "call redirected"
+    assert outputs == [expected, ], "output was sent to _f"
